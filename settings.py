b3a8c13496d75854086ef497ec7ab28947ace0ba
temirlanKabylbekov <temirlan.kabylbekov777@gmail.com>
2019-04-18 18:49:03 +0300

    Deadline is coming - remove stress tests and creating indexes.

--- settings.py
+++ settings.py
@@ -10,4 +10,3 @@ CURRENT_DIR = os.getcwd()
 MIGRATIONS_FOLDER = 'migrations'
 MIGRATION_INITIAL = os.path.join(CURRENT_DIR, MIGRATIONS_FOLDER, '1-initial.sql')
 MIGRATION_WITH_NEW_SCHEMA = os.path.join(CURRENT_DIR, MIGRATIONS_FOLDER, '2-with_new_schema.sql')
-MIGRATION_WITH_INDEXES = os.path.join(CURRENT_DIR, MIGRATIONS_FOLDER, '3-with_indexes.sql')
